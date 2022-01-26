910d1c164e71769d72f3d46f0a4c3c06b3c3c617
jbaker825 <jbaker825@gmail.com>
2022-01-26 15:20:04 -0600

    solved

--- index.js
+++ index.js
@@ -25,3 +25,17 @@ Solution:
 function solution(str) {
     return str.split('').reverse().join('')
 }
+
+___________________________________________________________________________
+
+    (8 kyu) Fake Binary
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'.Return the resulting string.
+
+    Note: input will never be an empty string
+
+Solution:
+
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
