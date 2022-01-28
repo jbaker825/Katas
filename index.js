6ab60cf1de2d34d4cc7281e09bb8df1d4e77a40b
jbaker825 <jbaker825@gmail.com>
2022-01-28 14:24:03 -0600

    kata

--- index.js
+++ index.js
@@ -70,3 +70,19 @@ function bmi(weight, height) {
     return "";
 }
 
+___________________________________________________________________________
+
+    (7 kyu) Sum of two lowest positive integers
+
+    Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.No floats or non - positive integers will be passed.
+
+For example, when an array is passed like[19, 5, 42, 2, 77], the output should be 7.
+
+
+Solution:
+
+function sumTwoSmallestNumbers(numbers) {
+    let [a, b] = numbers.sort((a, b) => a - b)
+    return a + b
+}
+
